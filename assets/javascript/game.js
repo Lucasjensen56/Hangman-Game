e731d43a2ee76da702aa9594abc4559f0ad36384
Lucas Jensen <33007506+Lucasjensen56@users.noreply.github.com>
2018-02-23 10:12:05 -0600

    Update game.js
    added psedocode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,76 @@
 
+/*PSEDUOCODE*/
+
+/*
+
+Pick a random word
+
+while the word has not ben guesses {
+  show the player their current progress
+  get a guess from the player 
+ 
+if the player wants to quit the game {
+Quit the game
+
+} Else if the guess is not a single letter {
+  tell the player to pick a single letter 
+  
+} Else {
+  If the guess is in the word {
+  Update the player's progress with the guess
+  }
+  }
+  }
+Congratulate the player on guessing the word
+
+
+
+
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*RANDOM Words*/
+
+var words = ['Alohomora', 'Basilisk', 'Azkaban', 'Dumbledore', 'Dementor', 'Fawkes', 'Hedwig',];
+
+function choooseWord () {
+  
+}
+
+
+/*TAKE PLAYER GUESSES*/
+
+
+
+/*CHECK THAT THE PLAYER'S GUESSES ARE A VALID LETTER*/
+
+
+
+/*KEEP TRACK OF LETTERS THE PLAYER HAS GUESS*/
+
+
+
+/*SHOW THE PLAYER THEIR PROGRESS*/
+
+
+
+
+
+/*FINISH WHEN THE PLAYER HAS GUESS THE WORD*/
+
