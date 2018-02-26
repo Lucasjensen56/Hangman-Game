8a2eda417f3191452214ad80d15bf1b65f972bf7
Lucas Jensen <lucasjensen@Wills-MacBook-Air.local>
2018-02-25 19:40:22 -0600

    added base code to javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,76 +1,123 @@
 
-/*PSEDUOCODE*/
+// var hello = alert("Do you like fish?");
+// console.log(hello);
 
-/*
+// array of random words
 
-Pick a random word
+var words = [
+  "alohomora", 
+  "basilisk", 
+  "azkaban", 
+  "dumbledore"
+  ];
 
-while the word has not ben guesses {
-  show the player their current progress
-  get a guess from the player 
- 
-if the player wants to quit the game {
-Quit the game
-
-} Else if the guess is not a single letter {
-  tell the player to pick a single letter 
-  
-} Else {
-  If the guess is in the word {
-  Update the player's progress with the guess
-  }
-  }
-  }
-Congratulate the player on guessing the word
+//   // Pick a random word
+  var word = words[Math.floor(Math.random() * words.length)];
 
 
-
-
-
-
-
-
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*RANDOM Words*/
-
-var words = ['Alohomora', 'Basilisk', 'Azkaban', 'Dumbledore', 'Dementor', 'Fawkes', 'Hedwig',];
-
-function choooseWord () {
-  
+// // Set up the Answer array
+var answerArray = [];
+for (var i = 0; i < word.length; i++) {
+  answerArray[i] = "_";
 }
 
-
-/*TAKE PLAYER GUESSES*/
-
+var remainingLetters = word.length;
 
 
-/*CHECK THAT THE PLAYER'S GUESSES ARE A VALID LETTER*/
+ // The game loop
+while (remainingLetters > 0) {
+// Show the player the progress
+  alert(answerArray.join(" "));
+
+// Take a guess from the player
+  var guess = prompt("Guess a letter, or click Cancel to stop player.");
+  if (guess === null) {
+    break;
+  } else if (guess.length !== 1) {
+    alert("Please enter a single letter.");
+  } else {
+    for (var j = 0; j < word.length; j++) {
+      if (word[j] === guess) {
+        answerArray[j] = guess;
+        remainingLetters--;
+      }
+    }
+  }
+}
+// // Show the answer 
+alert(answerArray.join(" "));
+alert("Good job! The answer was " + word);
 
 
 
-/*KEEP TRACK OF LETTERS THE PLAYER HAS GUESS*/
-
-
-
-/*SHOW THE PLAYER THEIR PROGRESS*/
 
 
 
 
+// // AnswerArray
 
-/*FINISH WHEN THE PLAYER HAS GUESS THE WORD*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /*PSEDUOCODE*/
+
+// /*
+
+// Pick a random word
+
+// while the word has not ben guesses {
+//   show the player their current progress
+//   get a guess from the player 
+ 
+// if the player wants to quit the game {
+// Quit the game
+
+// } Else if the guess is not a single letter {
+//   tell the player to pick a single letter 
+  
+// } Else {
+//   If the guess is in the word {
+//   Update the player's progress with the guess
+//   }
+//   }
+//   }
+// Congratulate the player on guessing the word
+
+// */
+
+
+
+// /*TAKE PLAYER GUESSES*/
+
+
+
+// /*CHECK THAT THE PLAYER'S GUESSES ARE A VALID LETTER*/
+
+
+
+// /*KEEP TRACK OF LETTERS THE PLAYER HAS GUESS*/
+
+
+
+// /*SHOW THE PLAYER THEIR PROGRESS*/
+
+
+
+
+
+// /*FINISH WHEN THE PLAYER HAS GUESS THE WORD*/
 
