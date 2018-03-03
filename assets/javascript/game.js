4ef36e36568028fc0d954cb641dff4d147ea2f73
Lucas Jensen <lucasjensen@er-10-132-235-154.wireless.umn.edu>
2018-03-03 09:52:58 -0600

    added querySelector that works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,21 @@
 
-// var hello = alert("Do you like fish?");
-// console.log(hello);
+// HTML elements to grab
+var lifeCount = document.querySelector("#lifeCount");
+var wrongLetters = document.querySelector("#wrongGuesses");
+var winsCount = document.querySelector("wins");
+var wins = 0;
+var word = "";
+var letterGuess = [];
 
-// array of random words
+
+// variable of a new game
+var score = 0;
+var chances = 10;
+var rightGuessBlanks = [];
+var wrongGuess = [];
+var numberofBlanks = 0;
+
+
 
 var words = [
   "alohomora", 
@@ -11,113 +24,92 @@ var words = [
   "dumbledore"
   ];
 
-//   // Pick a random word
-  var word = words[Math.floor(Math.random() * words.length)];
 
-
-// // Set up the Answer array
-var answerArray = [];
-for (var i = 0; i < word.length; i++) {
-  answerArray[i] = "_";
-}
-
-var remainingLetters = word.length;
-
-
- // The game loop
-while (remainingLetters > 0) {
-// Show the player the progress
-  alert(answerArray.join(" "));
-
-// Take a guess from the player
-  var guess = prompt("Guess a letter, or click Cancel to stop player.");
-  if (guess === null) {
-    break;
-  } else if (guess.length !== 1) {
-    alert("Please enter a single letter.");
-  } else {
-    for (var j = 0; j < word.length; j++) {
-      if (word[j] === guess) {
-        answerArray[j] = guess;
-        remainingLetters--;
-      }
-    }
+ var startGame = function() {
+  // game would set with score 0, 
+  chances = 10;
+  word = words[Math.floor(Math.random() * words.length)];
+  letterGuess = word.split("");
+  numberofBlanks = letterGuess.length;
+  rightGuessBlanks = [];
+  wrongGuess = [];
+  for (i = 0; i < numberofBlanks; i++) {
+    rightGuessBlanks.push("_")
   }
+  var solvedWordBlankes = document.querySelector("#wordBlanks");
+  solvedWordBlankes = rightGuessBlanks;
 }
-// // Show the answer 
-alert(answerArray.join(" "));
-alert("Good job! The answer was " + word);
+startGame();
+
+
+// array of random words
+
+// var words = [
+//   "alohomora", 
+//   "basilisk", 
+//   "azkaban", 
+//   "dumbledore"
+//   ];
+
+// //   // Pick a random word
+//   var word = words[Math.floor(Math.random() * words.length)];
+
+
+// // // Set up the Answer array
+// var answerArray = [];
+// for (var i = 0; i < word.length; i++) {
+//   answerArray[i] = "_";
+// }
+
+// var remainingLetters = word.length;
+
+// // remainingLetters = document.querySelector('remainingLetters');
+
+
+//  // The game loop
+// while (remainingLetters > 0) {
+// // Show the player the progress
+//   alert(answerArray.join("_ "));
 
 
 
+// // Take a guess from the player
+//   var guess = prompt("Guess a letter, or click Cancel to stop the game.");
+//   if (guess === null) {
+//     break;
+//   } else if (guess.length !== 1) {
+//     alert("Please enter a single letter.");
+//   } else {
+//     for (var j = 0; j < word.length; j++) {
+//       if (word[j] === guess) {
+//         answerArray[j] = guess;
+//         remainingLetters--;
+//         // alert("Good job! The answer was " + word);
+//       }
+//     }
+//   }
+// }
 
-
-
-
-// // AnswerArray
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// /*PSEDUOCODE*/
-
-// /*
-
-// Pick a random word
-
-// while the word has not ben guesses {
-//   show the player their current progress
-//   get a guess from the player 
- 
-// if the player wants to quit the game {
-// Quit the game
-
-// } Else if the guess is not a single letter {
-//   tell the player to pick a single letter 
   
-// } Else {
-//   If the guess is in the word {
-//   Update the player's progress with the guess
-//   }
-//   }
-//   }
-// Congratulate the player on guessing the word
-
-// */
+  
 
 
 
-// /*TAKE PLAYER GUESSES*/
 
-
-
-// /*CHECK THAT THE PLAYER'S GUESSES ARE A VALID LETTER*/
-
-
-
-// /*KEEP TRACK OF LETTERS THE PLAYER HAS GUESS*/
-
-
-
-// /*SHOW THE PLAYER THEIR PROGRESS*/
+// // // Show the answer 
+// alert(answerArray.join(" "));
+// alert("Good job! The answer was " + word);
 
 
 
 
 
-// /*FINISH WHEN THE PLAYER HAS GUESS THE WORD*/
+
+
+
+
+
+
+
+
 
